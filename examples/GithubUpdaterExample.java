59a86264064d2025d163fd6cb52a3e8bb88f336c
Daniel Englisch <xeroaustria@hotmail.com>
2017-05-18 11:09:59 +0200

    updated example

--- examples/GithubUpdaterExample.java
+++ examples/GithubUpdaterExample.java
@@ -3,11 +3,7 @@ package org.xeroserver.x0library.net;
 public class GithubUpdaterExample {
 
 	public static void main(String[] args) {
-		GithubUpdater updater = new GithubUpdater(
-				"<TOKEN>",
-				"<API-Resoucre>",
-				"<LocalVersion>"
-		);
+		GithubUpdater updater = new GithubUpdater("Personal Access Token", "DanielEnglisch/x0Library","1.0");
 		
 		if(updater.isUpdateAvailable()){
 			System.out.println("Update: " + updater.getDownloadLink());
