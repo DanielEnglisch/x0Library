69f009566cd7637907cd238b498c0096cb2c6964
Daniel Englisch <xeroaustria@hotmail.com>
2017-05-20 15:15:07 +0200

    added comments

--- examples/GithubUpdaterExample.java
+++ examples/GithubUpdaterExample.java
@@ -3,8 +3,11 @@ package org.xeroserver.x0library.net;
 public class GithubUpdaterExample {
 
 	public static void main(String[] args) {
+		
+		// Create an updater instance
 		GithubUpdater updater = new GithubUpdater("Personal Access Token", "DanielEnglisch/x0Library","1.0");
 		
+		// If there is an update available
 		if(updater.isUpdateAvailable()){
 			System.out.println("Update: " + updater.getDownloadLink());
 		}
--- examples/LoggerExample.java
+++ examples/LoggerExample.java
@@ -1,5 +1,6 @@
 package org.xeroserver.x0library.log;
 
+// Define a custom Logger which prints information differently
 class MyLogger extends Logger{
 	
 	public MyLogger(String name) {
@@ -14,12 +15,18 @@ class MyLogger extends Logger{
 
 public class LoggerExample {
 	
-
 	public static void main(String[] args) throws InterruptedException {
+		// Create two loggers
 		MyLogger root = new MyLogger("Root");
 		MyLogger sub = new MyLogger("Sub");
+		
+		// Set root as parent logger
 		sub.setParentLogger(root);
+		
+		// Enable timestamp
 		root.setTimestampEnabled(true);
+		
+		// Log messages
 		root.info("Hallo");
 		sub.error("Yolo");
 	}
