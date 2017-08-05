7cd3c2146b3069c7d2b86485d7f74b4a50cdbdd9
Daniel Englisch <xeroaustria@hotmail.com>
2017-08-05 23:11:32 +0200

    removed personal access token requirement

--- src/org/xeroserver/x0library/net/GithubUpdater.java
+++ src/org/xeroserver/x0library/net/GithubUpdater.java
@@ -9,7 +9,6 @@ import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Base64;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,26 +19,30 @@ import javax.swing.JLabel;
 import org.xeroserver.x0library.objtools.StringTools;
 
 /**
- * For checking if there is an update available and get the download link of the latest release of a Github repository
+ * For checking if there is an update available and get the download link of the
+ * latest release of a Github repository
+ * 
  * @author Xer0
  *
  */
 public class GithubUpdater {
 
 	private String remoteVersion, localVersion, downloadLink;
-	
+
 	/**
 	 * Constructor
-	 * @param token Generated "Personal Access Token" from https://github.com/settings/tokens
-	 * @param url Repository id "DanielEnglisch/x0_Library"
-	 * @param locVers Local application version
+	 * 
+	 * @param repo
+	 *            Repository id "DanielEnglisch/x0_Library"
+	 * @param locVers
+	 *            Local application version
 	 */
-	public GithubUpdater(String token, String resource, String locVers) {
+	public GithubUpdater(String repo, String locVers) {
 		this.localVersion = locVers;
-		
-		String jsonData = getOAuthedResource(token, resource);
 
-		//Parses the necessary data
+		String jsonData = getLatestRelease(repo);
+
+		// Parses the necessary data
 		Pattern version_p = Pattern.compile("tag_name\":\"[^\"]*\"");
 		Pattern download_p = Pattern.compile("browser_download_url\":\"[^\"]*\"");
 
@@ -54,25 +57,25 @@ public class GithubUpdater {
 		for (int i = 1; i < spl.length; i++)
 			downloadLink += spl[i] + ":";
 		downloadLink = StringTools.removeXCharsFromEnd(downloadLink, 1);
-		
+
 	}
-	
-	public boolean isUpdateAvailable(){
+
+	public boolean isUpdateAvailable() {
 		return !localVersion.equals(remoteVersion);
 	}
-	
-	public String getRemoteVersion(){
+
+	public String getRemoteVersion() {
 		return remoteVersion;
 	}
-	
-	public String getLocalVersion(){
+
+	public String getLocalVersion() {
 		return localVersion;
 	}
-	
-	public String getDownloadLink(){
+
+	public String getDownloadLink() {
 		return downloadLink;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "GithubUpdater [isUpdateAvailable()=" + isUpdateAvailable() + ", getRemoteVersion()="
@@ -83,9 +86,9 @@ public class GithubUpdater {
 	/**
 	 * Shows update dialog with a download button
 	 */
-	public void showUpdateDialog(){
-		if(isUpdateAvailable() && !GraphicsEnvironment.isHeadless()){
-			
+	public void showUpdateDialog() {
+		if (isUpdateAvailable() && !GraphicsEnvironment.isHeadless()) {
+
 			JFrame f = new JFrame("Update Available");
 			f.setLocationRelativeTo(null);
 			f.setSize(200, 150);
@@ -93,47 +96,43 @@ public class GithubUpdater {
 			f.setLayout(null);
 			JLabel rem = new JLabel("Remote Version: " + remoteVersion);
 			JLabel loc = new JLabel("Local Version: " + localVersion);
-			rem.setBounds(10, 10, 180 , 20);
-			loc.setBounds(10, 30, 180 , 20);
+			rem.setBounds(10, 10, 180, 20);
+			loc.setBounds(10, 30, 180, 20);
 			f.add(rem);
 			f.add(loc);
 			JButton downloadBtn = new JButton("Download");
 			downloadBtn.addActionListener(e -> {
-		        System.out.println("Opening: " + downloadLink);
-				      try {
-				        Desktop.getDesktop().browse(new URL(downloadLink).toURI());
-				      } catch (IOException e1) {
-				    	  e1.printStackTrace();
-				      } catch (URISyntaxException e2) {
-						e2.printStackTrace();
-					}
+				System.out.println("Opening: " + downloadLink);
+				try {
+					Desktop.getDesktop().browse(new URL(downloadLink).toURI());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (URISyntaxException e2) {
+					e2.printStackTrace();
+				}
 			});
 			downloadBtn.setBounds(10, 50, 180, 35);
 			f.add(downloadBtn);
 			f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			f.setVisible(true);
-		}else
-		{
+		} else {
 			System.out.println("There is an update available: " + downloadLink);
 		}
 	}
-	
+
 	/**
-	 * Function to request resources from the GithubAPI using OAuth
-	 * @param token Generated "Personal Access Token" from https://github.com/settings/tokens
-	 * @param repository id e.g. "DanielEnglisch/x0_Library"
+	 * Function to request resources from the GithubAPI
+	 * 
+	 * @param repository
+	 *            id e.g. "DanielEnglisch/x0_Library"
 	 * @return Returns requested data
 	 */
-	private String getOAuthedResource(String token, String url) {
+	private String getLatestRelease(String url) {
 		String ret = null;
-		url = "https://api.github.com/repos/"+url+"/releases/latest";
-
+		url = "https://api.github.com/repos/" + url + "/releases/latest";
 		try {
 			URL myURL = new URL(url);
 			URLConnection connection = myURL.openConnection();
-			token = token + ":x-oauth-basic";
-			String authString = "Basic " + Base64.getEncoder().encodeToString(token.getBytes());
-			connection.setRequestProperty("Authorization", authString);
 			InputStream webInputStream = connection.getInputStream();
 
 			BufferedReader in = null;
--- src/org/xeroserver/x0library/x0Library.java
+++ src/org/xeroserver/x0library/x0Library.java
@@ -3,7 +3,14 @@ package org.xeroserver.x0library;
 public final class x0Library{
 	
 	public static String getVersion(){
-		return "0.36";
+		return "0.37";
+	}
+	
+	public static String getChangelog(String version) {
+		switch(version) {
+		case "0.37": return "removed PersonalAccessToken from GithubUpdater";
+			default: return "No changelog available!";
+		}
 	}
 	
 }
